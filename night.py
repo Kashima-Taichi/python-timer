42779d3dfd86fe8f8a00cf1ea21a6fa7b3030984
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2020-09-16 21:55:49 +0900

    機能複数追加

--- night.py
+++ night.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import sys
 
 # タスクの変数宣言
 task1 = ['english study', '15m', 900]
@@ -16,8 +17,15 @@ bath = datetime(year=now.year, month=now.month,
                 day=now.day, hour=23, minute=15)
 bath_ts = bath.timestamp()
 
-# タスクに割り当てる時間を計算
+# 現在から風呂に入るまでの時間を算出する
 free_ts = bath_ts - now_ts
+
+# 風呂に入るまでの時間が1時間に満たない場合は、下記のメッセージを表示
+if free_ts < 3600:
+    print('You should study programming until you take bath.')
+    sys.exit()
+
+# 各タスクに割り当てる時間を計算
 free_ts = free_ts - task1[2]
 free_ts = free_ts - task2[2]
 task3_ts = free_ts * task3[1]
@@ -27,6 +35,7 @@ free_ts = free_ts - task4_ts
 
 # 計算した時間をターミナルに出力
 print('#############################')
+print('The current time is... ' + str(now.hour) + ':' + str(now.minute))
 print('belows are tasks list!')
 print(task1[0] + ' : ' + task1[1])
 print(task2[0] + ' : ' + task2[1])
